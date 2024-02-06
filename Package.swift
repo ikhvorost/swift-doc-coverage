95e608a9685961448fb96443a339d0500c458154
Iurii Khvorost <iurii.khvorost@dish.com>
2024-02-06 14:24:48 +0200

    Updated git workflow

--- Package.swift
+++ Package.swift
@@ -6,29 +6,33 @@ import PackageDescription
 let package = Package(
   name: "swift-doc-coverage",
   platforms: [
+    .iOS(.v13),
     .macOS(.v10_15),
+    .tvOS(.v13),
+    //.visionOS(.v1)
+    .watchOS(.v6)
   ],
   products: [
-    .executable(name: "swift-doc-coverage", targets: ["swift-doc-coverage"]),
-    .library(name: "SwiftDocCoverage", targets: ["SwiftDocCoverage"])
+    .library(name: "SwiftDocCoverage", targets: ["SwiftDocCoverage"]),
+    .executable(name: "swift-doc-coverage", targets: ["swift-doc-coverage"])
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-syntax.git", from: "509.1.1"),
     .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0")
   ],
   targets: [
-    .executableTarget(
-      name: "swift-doc-coverage",
-      dependencies: [
-        .target(name: "SwiftDocCoverage"),
-        .product(name: "ArgumentParser", package: "swift-argument-parser")
-      ]
-    ),
     .target(
       name: "SwiftDocCoverage",
       dependencies: [
         .product(name: "SwiftSyntax", package: "swift-syntax"),
         .product(name: "SwiftParser", package: "swift-syntax"),
+      ]
+    ),
+    .executableTarget(
+      name: "swift-doc-coverage",
+      dependencies: [
+        .target(name: "SwiftDocCoverage"),
+        .product(name: "ArgumentParser", package: "swift-argument-parser")
       ]
     ),
     .testTarget(
